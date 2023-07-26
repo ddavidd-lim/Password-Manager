ea2b27a848396977d1c7b75e1dbb285b685cb2aa
David <jxl1238@gmail.com>
2023-07-25 22:42:44 -0700

    Added loop for commands

--- manager.py
+++ manager.py
@@ -1,42 +1,80 @@
 import sqlite3
+import hashlib
+from tabulate import tabulate
 from account import Account
 
 connection = sqlite3.connect("passmanager.db")
 c = connection.cursor()
 
 
-c.execute("""CREATE TABLE IF NOT EXISTS accounts (
+c.execute(
+    """CREATE TABLE IF NOT EXISTS accounts (
           password text,
           user text,
           email text,
           site text
-        )""")
+        )"""
+)
 
 connection.commit()
 
+
 def insertAccount(account):
-  with connection:
-    c.execute("INSERT INTO accounts VALUES (:password, :user, :email, :site)",
-              {'password':account.password, 'user':account.username, 'email':account.email, 'site':account.appname})
+    with connection:
+        c.execute(
+            "INSERT INTO accounts VALUES (:password, :user, :email, :site)",
+            {
+                "password": account.password,
+                "user": account.username,
+                "email": account.email,
+                "site": account.appname,
+            },
+        )
+
 
 def getAccountbySite(site):
-  c.execute("SELECT * FROM accounts WHERE site=site")
+    c.execute("SELECT * FROM accounts WHERE site=:site", {"site": site})
+
 
 def updateAccountPass(account, password):
-  with connection:
-    c.execute("""UPDATE accounts SET password = :password
+    with connection:
+        c.execute(
+            """UPDATE accounts SET password = :password
               WHERE user = :user AND email = :email AND site=:site""",
-              {'user':account.username, 'email':account.email, 'site':account.appname, 'password':password})
+            {
+                "user": account.username,
+                "email": account.email,
+                "site": account.appname,
+                "password": password,
+            },
+        )
+    account.password = password
 
-def printResult():
-  results = c.fetchall() #list of results
-  for r in results:
-    print(f">> [{r[2]}]- user:{r[1]}, password:{r[0]}")
+
+def print_query_results():
+    rows = c.fetchall()
+    if not rows:
+        print("No results found.")
+    else:
+        headers = [i[0] for i in c.description]
+        print(tabulate(rows, headers=headers, tablefmt="pretty"))
+
 
 acc_1 = Account("me", "securepassword", "me@gmail.com", "google")
 
 # insertAccount(acc_1)
-getAccountbySite("google")
-printResult()
+# getAccountbySite("google")
+# print_query_results()
+
+command = "none"
+while command != "exit" or command != "quit":
+    command = str(input(">>Input master password\n"))
+    if command != "master":
+        pass
+    else:
+        print("ok you did it correctly")
+        while command != "exit" or command != "quit":
+            command = str(input(">>Input the site you would like to receive credentials of.\n"))
+
 
 connection.close()
